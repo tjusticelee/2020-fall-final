8b0a672d69852c1d758785557f495a63c1da3208
Justice Lee <txawjcojlee@gmail.com>
2020-11-24 13:28:01 -0500

    change files to make login work

---
+++ general/login.php
@@ -0,0 +1,34 @@
+<?php
+
+echo <<< "EOT"
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+
+  <body>
+    <h1>Login</h1>
+
+    <form class="" action="bulkwork/login-work.php" method="post">
+      <label for="email">E-mail:</label>
+      <input type="text" name="email" value="">
+        <br>
+      <label for="password">Password:</label>
+      <input type="text" name="password1" value="">
+
+      <input id="submit" type="submit" value="Submit">
+    </form>
+
+  </body>
+</html>
+EOT;
+
+session_start();
+if(isset($_SESSION['error'])){
+  echo $_SESSION['error'];
+  unset( $_SESSION['error'] );
+}
+
+?>
