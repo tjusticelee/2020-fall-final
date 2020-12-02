fa483516b94d279abdd082e07c4b218b809fb10b
Justice Lee <txawjcojlee@gmail.com>
2020-12-02 11:34:24 -0500

    make js function where extra info is needed if patient

--- general/bulkwork/xtrainfo.js
+++ general/bulkwork/xtrainfo.js
@@ -1,5 +1,23 @@
-fuction moreinfo(role){
+function moreinfo(role) {
   const form = document.getElementsByClassName('register')[0];
   // in register.php puts the form with class of register in a variable called form
-  if role.value
+  if (role.value == 5) {
+    const inputgenerate = (name, text) => {
+      const patientInputLabel = document.createElement("label");
+      patientInputLabel.setAttribute("for", name);
+      patientInputLabel.appendChild(document.createTextNode(text));
+
+      const patientInput = document.createElement("input");
+      patientInput.setAttribute("type", "text");
+      patientInput.setAttribute("name", name);
+      patientInput.setAttribute("class", "patient");
+
+      const submit = document.getElementById("submit");
+      form.insertBefore(patientInputLabel, submit);
+      form.insertBefore(patientInput, submit);
+    }
+    inputgenerate('familycode', "Family Code");
+    inputgenerate('emer_contact', "Emergency Contact");
+    inputgenerate('relation', 'Relation');
+  }
 }
--- general/register.php
+++ general/register.php
@@ -6,45 +6,66 @@ echo <<< "EOT"
   <head>
     <meta charset="utf-8">
     <title>Register</title>
+    <script type="text/javascript" src="bulkwork/xtrainfo.js"></script>
   </head>
 
   <body>
 
     <h1>Register</h1>
+
       <br>
-    <form class="register" action="index.html" method="post">
+
+    <form class="form-style register" action="index.html" method="post">
+
       <label for="role">Role</label>
-      <select name="oper" id="oper">
-        <option value="1">Admin</option>
-        <option value="2">Doctor</option>
-        <option value="3">Supervisor</option>
-        <option value="4">Care giver</option>
-        <option value="5">Patient</option>
-        <option value="6">Family member</option>
+      <select onchange="moreinfo(role)" name="role">
+        <option value=1 >Admin</option>
+        <option value=2 >Doctor</option>
+        <option value=3 >Supervisor</option>
+        <option value=4 >Care giver</option>
+        <option value=5 >Patient</option>
+        <option value=6 >Family member</option>
       </select>
+
         <br>
+
       <label for="first_name">First Name:</label>
       <input type="text" name="first_name" value="">
+
         <br>
+
       <label for="last_name">Last Name:</label>
       <input type="text" name="last_name" value="">
+
         <br>
+
       <label for="email">email:</label>
       <input type="text" name="email" value="">
+
         <br>
+
       <label for="phone_num">Phone Number:</label>
       <input type="text" name="phone_num" value="">
+
         <br>
+
       <label for="DOB">Date of Birth:</label>
       <input type="date" name="DOB" value="">
+
         <br>
+
       <label for="password">Password:</label>
       <input type="text" name="password" value="">
+
         <br>
+
       <label for="vpassword">Verify password:</label>
       <input type="text" name="password1" value="">
+
         <br>
+
       <input id="submit" type="submit" value="Submit">
+
     </form>
   </body>
 </html>
