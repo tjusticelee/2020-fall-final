85d48e9a85e85f10660ea156eb07777c6f7d6d27
Justice Lee <txawjcojlee@gmail.com>
2020-12-08 18:02:21 -0500

    finish back end add role

--- home-pages/admin-pages/role-add-work.php
+++ home-pages/admin-pages/role-add-work.php
@@ -5,7 +5,21 @@ require_once '../../general/bulkwork/auth.php';
 
 session_start();
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && auth([1], $link)) {
+  $name = $_POST['name'];
+  $level = $_POST['level'];
 
 
+  if ($stmt = $link->prepare('INSERT INTO role (name, access_lvl)
+                            VALUES (?,?)')) {
+
+      $stmt->bind_param('si', $name, $level);
+      $stmt->execute();
+      $stmt->close();
+  }
+
+}
+
+header('Location: role.php');
 
 ?>
